b8b0e7f41678ec0625e8e3432baa81cb85acc181
kelly-edling <38821134+kelly-edling@users.noreply.github.com>
2018-05-27 18:20:26 -0700

    Homework files
    all about Node

---
+++ websvr.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World, my name is Kelly Edling');
+}).listen(8001);
